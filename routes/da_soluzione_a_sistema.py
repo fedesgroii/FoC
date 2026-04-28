1c06807d6bf879d7cc53bb514523143fe082c3f0
fedesgroii <federico.sgroi@students.uniroma2.eu>
2026-04-28 17:34:01 +0200

    2 pt (errori impaginazione

--- routes/da_soluzione_a_sistema.py
+++ routes/da_soluzione_a_sistema.py
@@ -184,19 +184,16 @@ def api_da_soluzione_a_sistema():
 
         # FIX IMPAGINAZIONE SEZIONE 5
         # Uso \\\\ per andare a capo tra le matrici
-        latex_steps.append({
-            "title": "Matrice inversa",
-            "content": (
-                r"\text{Matrice dei cofattori: } \mathrm{Cof}(A) = " + "\n" +
-                to_latex(cofactor_matrix) + "\n" +
-                r"\\ \\ " +
-                r"\text{Matrice aggiunta: } \mathrm{Adj}(A) = \mathrm{Cof}(A)^T = " + "\n" +
-                to_latex(adj_A) + "\n" +
-                r"\\ \\ " +
-                r"A^{-1} = \frac{1}{\det(A)} \cdot \mathrm{Adj}(A) = " + "\n" +
-                to_latex(A_inv)
-            )
-        })
+       latex_steps.append({
+    "title": "Matrice inversa",
+    "content": (
+        r"\begin{align*}" +
+        r"\text{Matrice dei cofattori: } \mathrm{Cof}(A) &= " + to_latex(cofactor_matrix) + r"\\" +
+        r"\text{Matrice aggiunta: } \mathrm{Adj}(A) &= \mathrm{Cof}(A)^T = " + to_latex(adj_A) + r"\\" +
+        r"A^{-1} &= \frac{1}{\det(A)} \cdot \mathrm{Adj}(A) = " + to_latex(A_inv) +
+        r"\end{align*}"
+    )
+})
 
 
 
